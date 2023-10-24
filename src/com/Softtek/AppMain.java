d3fc2f58d2945e9c23bd9bd8126f4c77368b9e56
Lucia <luciapgarc@ext.inditex.com>
2023-10-24 17:50:27 +0200

    con apellido

--- src/com/Softtek/AppMain.java
+++ src/com/Softtek/AppMain.java
@@ -1,10 +1,18 @@
 package com.Softtek;
 
+import com.Softtek.models.Alumno;
+import com.Softtek.models.Direccion;
+
 public class AppMain {
 
 	public static void main(String[] args) {
 	 System.out.println("Bienvenidos");
 
+	 Alumno alumno = new Alumno(1, "Pepe", 6.5, new Direccion("Mayor",5 , "Madrid"));
+	 System.out.println(alumno);
+	 
+	 Alumno alumno2 = new Alumno(2, "Maria", 6.5, new Direccion("Real", 11 , "Toledo"));
+	 System.out.println(alumno2);
 	}
 
 }
--- src/com/Softtek/models/Alumno.java
+++ src/com/Softtek/models/Alumno.java
@@ -4,33 +4,54 @@ public class Alumno {
 	private int numAlumno;
 	private String nombre;
 	private double nota;
-	private Direccion direccion;
+	private String apellido;
+	private Direccion direccion;	
 	
-	
-	@Override
-	public String toString() {
-		return "Alumno [numAlumno=" + numAlumno + ", nombre=" + nombre + ", nota=" + nota + ", direccion=" + direccion
-				+ "]";
-	}
-	public Direccion getDireccion() {
-		return direccion;
-	}
-	public void setDireccion(Direccion direccion) {
-		this.direccion = direccion;
-	}
-	public Alumno(Direccion direccion) {
-		super();
-		this.direccion = direccion;
-	}
 	public Alumno() {
 		super();
 	}
+
+	public Alumno(int numAlumno, String nombre, double nota, Direccion direccion) {
+		super();
+		this.numAlumno = numAlumno;
+		this.nombre = nombre;
+		this.nota = nota;
+		this.direccion = direccion;
+	}
+
 	public Alumno(int numAlumno, String nombre, double nota) {
 		super();
 		this.numAlumno = numAlumno;
 		this.nombre = nombre;
 		this.nota = nota;
 	}
+	
+	
+	public Alumno(int numAlumno, String nombre, double nota, String apellido, Direccion direccion) {
+		super();
+		this.numAlumno = numAlumno;
+		this.nombre = nombre;
+		this.nota = nota;
+		this.apellido = apellido;
+		this.direccion = direccion;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public Direccion getDireccion() {
+		return direccion;
+	}
+		
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+	}	
+	
 	public int getNumAlumno() {
 		return numAlumno;
 	}
@@ -48,6 +69,12 @@ public class Alumno {
 	}
 	public void setNota(double nota) {
 		this.nota = nota;
+	}
+	
+	@Override
+	public String toString() {
+		return "Alumno [numAlumno=" + numAlumno + ", nombre=" + nombre + ", nota=" + nota + ", apellido=" + apellido
+				+ ", direccion=" + direccion + "]";
 	}
 
 }
